7f3dced260dc4d11e07f4efe1d79cfb7a501a260
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-11 08:42:51 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,31 +4,35 @@ public class Runner {
 
     public static void main(String[] args)
     {
+        String[] stringArr = {"Nelson", "John", "Bob", "Watermelon", "Array"};
+        double[] doubleArr = {8.5, 6.6, 6.9, 1.1, 8.8, 9.9};
         int[] testArr = {8,6,7,5,3,0,9,10,1,2,3};
+
         SortingUtil.copyIntArray(testArr);
-        int[] bubbleSortArr = SortingUtil.copyIntArray(testArr);
-        int[] selectionSortArr = SortingUtil.copyIntArray(testArr);
+        String[] bubbleSortArr = SortingUtil.copyStringArray(stringArr);
+        double[] selectionSortArr = SortingUtil.copyDoubleArray(doubleArr);
         int[] insertionSortArr = SortingUtil.copyIntArray(testArr);
+
         System.out.print("Before Sorting: ");
         for(int num:testArr)
             System.out.print(num + " ");
         System.out.println();
 
-        InPlaceSorts.bubbleSort(bubbleSortArr);
+        InPlaceSorts.bubbleSort(stringArr);
 
         System.out.print("After Bubble Sort: ");
-        for(int num:bubbleSortArr)
-            System.out.print(num + " ");
+        for(String String:bubbleSortArr)
+            System.out.print(String + " ");
         System.out.println();
 
         InPlaceSorts.selectionSort(selectionSortArr);
 
         System.out.print("After Selection Sort: ");
-        for(int num:selectionSortArr)
+        for(double num:selectionSortArr)
             System.out.print(num + " ");
         System.out.println();
 
-        InPlaceSorts.insertionSort(selectionSortArr);
+        InPlaceSorts.insertionSort(insertionSortArr);
 
         System.out.print("After Insertion Sort: ");
         for(int num:insertionSortArr)

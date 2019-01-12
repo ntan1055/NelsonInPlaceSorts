77e6a12e902874cfabfc6bf7ebc7f3fecb725380
ntan1055 <43207464+ntan1055@users.noreply.github.com>
2019-01-11 21:36:16 -0500

    Update Runner.java

--- src/com/company/Runner.java
+++ src/com/company/Runner.java
@@ -4,40 +4,56 @@ public class Runner {
 
     public static void main(String[] args)
     {
-        String[] stringArr = {"Nelson", "John", "Bob", "Watermelon", "Array"};
+        String[] stringArr = {"Nelson", "Bob", "Watermelon", "Humpty-Dumpty"};
         double[] doubleArr = {8.5, 6.6, 6.9, 1.1, 8.8, 9.9};
-        int[] testArr = {8,6,7,5,3,0,9,10,1,2,3};
+        int[] intArr = {8,6,7,5,3,0,9,10,1,2,3};
 
-        SortingUtil.copyIntArray(testArr);
         String[] bubbleSortArr = SortingUtil.copyStringArray(stringArr);
         double[] selectionSortArr = SortingUtil.copyDoubleArray(doubleArr);
-        int[] insertionSortArr = SortingUtil.copyIntArray(testArr);
+        int[] insertionSortArr = SortingUtil.copyIntArray(intArr);
 
-        System.out.print("Before Sorting: ");
-        for(int num:testArr)
-            System.out.print(num + " ");
-        System.out.println();
-
-        InPlaceSorts.bubbleSort(stringArr);
-
-        System.out.print("After Bubble Sort: ");
+        System.out.print("Before Bubble Sorting: ");
         for(String String:bubbleSortArr)
             System.out.print(String + " ");
         System.out.println();
 
+        InPlaceSorts.bubbleSort(bubbleSortArr);
+
+        System.out.print("After Bubble Sorting: ");
+        for(String String:bubbleSortArr)
+            System.out.print(String + " ");
+        System.out.println();
+
+        System.out.print("Before Selection Sorting: ");
+        for(double num:selectionSortArr)
+            System.out.print(num + " ");
+        System.out.println();
+
         InPlaceSorts.selectionSort(selectionSortArr);
 
-        System.out.print("After Selection Sort: ");
+        System.out.print("After Selection Sorting: ");
         for(double num:selectionSortArr)
+            System.out.print(num + " ");
+        System.out.println();
+
+        System.out.print("Before Insertion Sorting: ");
+        for(int num:insertionSortArr)
             System.out.print(num + " ");
         System.out.println();
 
         InPlaceSorts.insertionSort(insertionSortArr);
 
-        System.out.print("After Insertion Sort: ");
+        System.out.print("After Insertion Sorting: ");
         for(int num:insertionSortArr)
             System.out.print(num + " ");
         System.out.println();
+
+        SortingUtil.checkDoubleSum(doubleArr, selectionSortArr);
+        SortingUtil.checkIntSum(intArr, insertionSortArr);
+
+        SortingUtil.areStringsSorted(bubbleSortArr);
+        SortingUtil.areDoubleValuesSorted(selectionSortArr);
+        SortingUtil.areIntValuesSorted(insertionSortArr);
 
         long time = System.nanoTime();
         SortingUtil.randIntArr(11);
